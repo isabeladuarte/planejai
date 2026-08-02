ece0b1c7d6241f313ce10cb2fb4fe31ee3fb6981
Isabela Duarte <isabeladuartelbg@gmail.com>
2026-08-02 16:15:13 -0300

    feat: crate app header and base layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { Button } from './components/shared/shared/Button';
+import { PiggyBank } from 'lucide-react';
+import { RootLayout } from './components/shared/layout/RootLayout';
 
 export const router = createBrowserRouter([
 	{
+		element: <RootLayout />, // Exibe para todos os caminhos
 		children: [
 			{
 				path: '/',
-				element: <h1>Formulário de Simulação</h1>,
+				element:
+					<>
+						<h1>Formulário de Simulação</h1>
+						<Button variant="primary" icon={PiggyBank} className="w-full">Clique aqui</Button>
+					</>
 			},
 			{
 				path: '/resultado',
